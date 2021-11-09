ac8b6fd7bf6e8a36927cb4ae342a797130bd2bf0
PedroLeiva <pedro.leiva@bandtec.com.br>
2021-11-09 18:15:36 -0300

    [backend]Layout txt

--- aplicacao/backend/ListaResenha/src/main/java/ListaAgendamento/Teste.java
+++ aplicacao/backend/ListaResenha/src/main/java/ListaAgendamento/Teste.java
@@ -1,9 +1,9 @@
 package ListaAgendamento;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import ListaQuadra.Quadra;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Teste {
@@ -108,9 +108,142 @@ public class Teste {
         }
     }
 
+    public static void gravaRegistro(String registro,String nomeArq){
+        BufferedWriter saida = null;
+
+        //Abre o arquivo
+        try {
+            saida = new BufferedWriter(new FileWriter(nomeArq, true));
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao abrir o arquivo: " + erro.getMessage());
+        }
+
+        //Grava o registro e fecha o arquivo
+        try {
+            saida.append(registro + "\n");
+            saida.close();
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao gravar o arquivo: " + erro.getMessage());
+        }
+
+    }
+
+    public static void gravaArquivoTxt(List<Agendamento> lista, String nomeArq){
+
+        //Monta o registro de reader
+        String header = "00AGENDAMENTO";
+        Date dataDeHoje = new Date();
+        SimpleDateFormat formataData = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        header += formataData.format(dataDeHoje);
+        header += "01";
+
+
+        //Grava o header
+        gravaRegistro(header, nomeArq);
+
+
+        Integer contaRegDados = 0;
+
+        //Monta e grava o corpo
+        for (Agendamento a:lista) {
+            String corpo = "02";
+            corpo += String.format("%04d",a.getIdJogador());
+            corpo += String.format("%04d",a.getIdQuadra());
+            corpo += String.format("%06.2f",a.getPreco());
+            corpo += String.format("%19.19s",a.getHoraMarcada());
+
+            contaRegDados++;
+
+            gravaRegistro(corpo, nomeArq);
+        }
+
+
+        //Monta e grava o registro de trailer
+        String trailer = "01";
+        trailer += String.format("%05d",contaRegDados);
+
+        gravaRegistro(trailer, nomeArq);
+    }
+
+    public static void leArquivoTxt(String nomeArq){
+        BufferedReader entrada = null;
+        String registro,tipoRegistro;
+        String horaMarcada;
+        Integer idJogador,idQuadra;
+        Double preco;
+        Integer qtdResgistrosGravados;
+
+        List<Agendamento> listaLida = new ArrayList<>();
+
+        //Abre o arquivo
+        try {
+            entrada = new BufferedReader(new FileReader(nomeArq));
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao abrir o arquivo: " + erro.getMessage());
+        }
+
+
+        //Lê o arquivo
+        try {
+            //Lê o primeiro registro do arquivo
+            registro = entrada.readLine();
+            while (registro != null){
+                //Enquanto não chegou ao final do arquivo
+                tipoRegistro = registro.substring(0,2);
+
+                if (tipoRegistro.equals("00")){
+                    System.out.println("\nÉ um registro de header");
+                    System.out.println("Tipo de arquivo: " + registro.substring(2,13));
+                    System.out.println("Data/hora de gravação: " + registro.substring(13,32));
+                    System.out.println("Versão do documento: " + registro.substring(32,34));
+                }
+                else if(tipoRegistro.equals("01")){
+                    System.out.println("É um registro de trailer");
+                    qtdResgistrosGravados = Integer.valueOf(registro.substring(2,7));
+                    if (qtdResgistrosGravados == listaLida.size()){
+                        System.out.println("Quantidade de registros gravados compatível com quantidade lida");
+                    }
+                    else{
+                        System.out.println("Quantidade de registro gravados imcompatível com quantidade lida");
+                    }
+
+                }
+                else if(tipoRegistro.equals("02")){
+                    System.out.println("É um registro de corpo");
+                    idJogador = Integer.valueOf(registro.substring(2,6));
+                    idQuadra = Integer.valueOf(registro.substring(6,10));
+                    preco = Double.valueOf(registro.substring(10,16).replace(',','.'));
+                    horaMarcada = registro.substring(16,35);
+
+
+
+                    listaLida.add(new Agendamento(idJogador,idQuadra,preco,horaMarcada));
+                }
+                else{
+                    System.out.println("Tipo de registro inválido");
+                }
+                //Le o proximo registro
+                registro = entrada.readLine();
+            }
+            entrada.close();
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao ler o arquivo: " + erro.getMessage());
+        }
+
+        System.out.println("\nConteúdo lido do arquivo");
+        for (Agendamento a:listaLida) {
+            System.out.println(a);
+        }
+    }
+
     public static void main(String[] args) {
 
         ListaObj<Agendamento> lista = new ListaObj<>(7);
+        List<Agendamento> listaAgendamento = new ArrayList<>();
 
         lista.adiciona(new Agendamento(1,1,150.0,"2021-10-13 13:00:00"));
         lista.adiciona(new Agendamento(2,2,130.5,"2021-10-13 13:00:00"));
@@ -119,10 +252,21 @@ public class Teste {
         lista.adiciona(new Agendamento(5,2,130.5,"2021-10-13 14:00:00"));
         lista.adiciona(new Agendamento(6,1,150.0,"2021-10-13 15:00:00"));
 
+        listaAgendamento.add(new Agendamento(1,1,150.0,"2021-10-13 13:00:00"));
+        listaAgendamento.add(new Agendamento(2,2,130.5,"2021-10-13 13:00:00"));
+        listaAgendamento.add(new Agendamento(3,3,110.0,"2021-10-13 13:00:00"));
+        listaAgendamento.add(new Agendamento(4,1,150.0,"2021-10-13 14:00:00"));
+        listaAgendamento.add(new Agendamento(5,2,130.5,"2021-10-13 14:00:00"));
+        listaAgendamento.add(new Agendamento(6,1,150.0,"2021-10-13 15:00:00"));
+
         lista.exibe();
 
         gravaArquivoCsv(lista,"agend");
 
         leExibeArquivoCsv("agend");
+
+        gravaArquivoTxt(listaAgendamento, "agend");
+
+        leArquivoTxt("agend");
     }
 }
--- aplicacao/backend/ListaResenha/src/main/java/ListaQuadra/Teste.java
+++ aplicacao/backend/ListaResenha/src/main/java/ListaQuadra/Teste.java
@@ -1,13 +1,8 @@
 package ListaQuadra;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Formatter;
-import java.util.FormatterClosedException;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class Teste {
 
@@ -111,20 +106,163 @@ public class Teste {
         }
     }
 
+    public static void gravaRegistro(String registro,String nomeArq){
+        BufferedWriter saida = null;
+
+        //Abre o arquivo
+        try {
+            saida = new BufferedWriter(new FileWriter(nomeArq, true));
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao abrir o arquivo: " + erro.getMessage());
+        }
+
+        //Grava o registro e fecha o arquivo
+        try {
+            saida.append(registro + "\n");
+            saida.close();
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao gravar o arquivo: " + erro.getMessage());
+        }
+
+    }
+
+    public static void gravaArquivoTxt(List<Quadra> lista, String nomeArq){
+
+        //Monta o registro de reader
+        String header = "00QUADRA";
+        Date dataDeHoje = new Date();
+        SimpleDateFormat formataData = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        header += formataData.format(dataDeHoje);
+        header += "01";
+
+
+        //Grava o header
+        gravaRegistro(header, nomeArq);
+
+
+        Integer contaRegDados = 0;
+
+        //Monta e grava o corpo
+        for (Quadra q:lista) {
+            String corpo = "02";
+            corpo += String.format("%04d",q.getId());
+            corpo += String.format("%-8.8s",q.getModalidade());
+            corpo += String.format("%02d",q.getNumeroQuadra());
+            corpo += String.format("%b",q.getDisponivel());
+
+            contaRegDados++;
+
+            gravaRegistro(corpo, nomeArq);
+        }
+
+
+        //Monta e grava o registro de trailer
+        String trailer = "01";
+        trailer += String.format("%05d",contaRegDados);
+
+        gravaRegistro(trailer, nomeArq);
+    }
+
+    public static void leArquivoTxt(String nomeArq){
+        BufferedReader entrada = null;
+        String registro,tipoRegistro;
+        String modalidade;
+        Integer id,numeroQuadra;
+        Boolean disponivel;
+        Integer qtdResgistrosGravados;
+
+        List<Quadra> listaLida = new ArrayList<>();
+
+        //Abre o arquivo
+        try {
+            entrada = new BufferedReader(new FileReader(nomeArq));
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao abrir o arquivo: " + erro.getMessage());
+        }
+
+
+        //Lê o arquivo
+        try {
+            //Lê o primeiro registro do arquivo
+            registro = entrada.readLine();
+            while (registro != null){
+                //Enquanto não chegou ao final do arquivo
+                tipoRegistro = registro.substring(0,2);
+
+                if (tipoRegistro.equals("00")){
+                    System.out.println("\n" +
+                            "É um registro de header");
+                    System.out.println("Tipo de arquivo: " + registro.substring(2,8));
+                    System.out.println("Data/hora de gravação: " + registro.substring(8,27));
+                    System.out.println("Versão do documento: " + registro.substring(27,29));
+                }
+                else if(tipoRegistro.equals("01")){
+                    System.out.println("É um registro de trailer");
+                    qtdResgistrosGravados = Integer.valueOf(registro.substring(2,7));
+                    if (qtdResgistrosGravados == listaLida.size()){
+                        System.out.println("Quantidade de registros gravados compatível com quantidade lida");
+                    }
+                    else{
+                        System.out.println("Quantidade de registro gravados imcompatível com quantidade lida");
+                    }
+
+                }
+                else if(tipoRegistro.equals("02")){
+                    System.out.println("É um registro de corpo");
+                    id = Integer.valueOf(registro.substring(2,6));
+                    modalidade = registro.substring(6,14).trim();
+                    numeroQuadra = Integer.valueOf(registro.substring(14,16));
+                    disponivel = Boolean.valueOf(registro.substring(16,18));
+
+
+
+                    listaLida.add(new Quadra(id,modalidade,numeroQuadra,disponivel));
+                }
+                else{
+                    System.out.println("Tipo de registro inválido");
+                }
+                //Le o proximo registro
+                registro = entrada.readLine();
+            }
+            entrada.close();
+        }
+        catch (IOException erro){
+            System.out.println("Erro ao ler o arquivo: " + erro.getMessage());
+        }
+
+        System.out.println("\nConteúdo lido do arquivo");
+        for (Quadra q:listaLida) {
+            System.out.println(q);
+        }
+    }
+
     public static void main(String[] args) {
 
         ListaQuadra.ListaObj<Quadra> lista = new ListaObj<>(7);
+        List<Quadra> listaQuadra = new ArrayList<>();
 
         lista.adiciona(new Quadra(1,"Futebol",7,true));
         lista.adiciona(new Quadra(2,"Basquete",2,true));
         lista.adiciona(new Quadra(3,"Tenis",3,true));
         lista.adiciona(new Quadra(4,"Volei",5,false));
 
-        lista.exibe();
+        listaQuadra.add(new Quadra(1,"Futebol",7,true));
+        listaQuadra.add(new Quadra(2,"Basquete",2,true));
+        listaQuadra.add(new Quadra(3,"Tenis",3,true));
+        listaQuadra.add(new Quadra(4,"Volei",5,false));
 
+//        lista.exibe();
+//
         gravaArquivoCsv(lista,"quad");
 
         leExibeArquivoCsv("quad");
+
+        gravaArquivoTxt(listaQuadra, "quad.txt");
+
+        leArquivoTxt("quad.txt");
     }
 }
 
