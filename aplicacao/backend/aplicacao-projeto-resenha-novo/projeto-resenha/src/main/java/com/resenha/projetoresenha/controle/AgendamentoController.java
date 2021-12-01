b718e817eadbef1c1f13d44fc787795945e35fa9
akzv-oliveira <vinicius.soliveira@bandtec.com.br>
2021-12-01 04:00:44 -0300

    [Bug] Corrige chamada do CrossOrigin nas requisições de agendamento

--- aplicacao/backend/aplicacao-projeto-resenha-novo/projeto-resenha/src/main/java/com/resenha/projetoresenha/controle/AgendamentoController.java
+++ aplicacao/backend/aplicacao-projeto-resenha-novo/projeto-resenha/src/main/java/com/resenha/projetoresenha/controle/AgendamentoController.java
@@ -21,31 +21,29 @@ import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/agendamentos")
+@CrossOrigin
 public class AgendamentoController {
 
+    @Autowired
+    private AgendamentoRepository repository;
 
-        @Autowired
-        private AgendamentoRepository repository;
-
-        @CrossOrigin
-        @GetMapping
-        public ResponseEntity getAgendamento() {
-            List<Agendamento> agendamentoLista = repository.findAll();
-            if (agendamentoLista.isEmpty()) {
-                return ResponseEntity.status(204).build();
-            }
-            FilaObj<Agendamento> agendamentoFilaObj  = new FilaObj<>(agendamentoLista.size());
-            LocalDateTime dataAtual = LocalDateTime.now();
-            agendamentoLista.stream()
-                    .filter((agendamento) -> agendamento.getHora_Marcada().isAfter(dataAtual) ||
-                            agendamento.getHora_Marcada().equals(dataAtual))
-                    .sorted(Comparator.comparing(Agendamento::getHora_Marcada, Comparator.naturalOrder()))
-                    .forEach(agendamentoFilaObj::insert);
-            return ResponseEntity.status(200).body(agendamentoFilaObj.toList());
+    @GetMapping
+    public ResponseEntity getAgendamento() {
+        List<Agendamento> agendamentoLista = repository.findAll();
+        if (agendamentoLista.isEmpty()) {
+            return ResponseEntity.status(204).build();
         }
+        FilaObj<Agendamento> agendamentoFilaObj = new FilaObj<>(agendamentoLista.size());
+        LocalDateTime dataAtual = LocalDateTime.now();
+        agendamentoLista.stream()
+                .filter((agendamento) -> agendamento.getHora_Marcada().isAfter(dataAtual) ||
+                        agendamento.getHora_Marcada().equals(dataAtual))
+                .sorted(Comparator.comparing(Agendamento::getHora_Marcada, Comparator.naturalOrder()))
+                .forEach(agendamentoFilaObj::insert);
+        return ResponseEntity.status(200).body(agendamentoFilaObj.toList());
+    }
 
     @GetMapping("/ocorridos")
     public ResponseEntity getAgendamentoOcorrido() {
@@ -53,7 +51,7 @@ public class AgendamentoController {
         if (agendamentoLista.isEmpty()) {
             return ResponseEntity.status(204).build();
         }
-        PilhaObj<Agendamento> agendamentoFilaObj  = new PilhaObj<>(agendamentoLista.size());
+        PilhaObj<Agendamento> agendamentoFilaObj = new PilhaObj<>(agendamentoLista.size());
         LocalDateTime dataAtual = LocalDateTime.now();
         agendamentoLista.stream()
                 .filter((agendamento) -> agendamento.getHora_Marcada().isBefore(dataAtual) ||
@@ -63,62 +61,63 @@ public class AgendamentoController {
         return ResponseEntity.status(200).body(agendamentoFilaObj.toList());
     }
 
-        @PostMapping
-        public ResponseEntity postAgendamento(@RequestBody Agendamento novoAgendamento) {
-            repository.save(novoAgendamento);
-            return ResponseEntity.status(201).build();
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity getAgendamento(@PathVariable Integer id) {
-            return ResponseEntity.of(repository.findById(id));
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity deleteAgendamento(@PathVariable Integer id) {
-            if (repository.existsById(id)) {
-                repository.deleteById(id);
-                return ResponseEntity.status(200).build();
-            }
-            return ResponseEntity.status(404).build();
-        }
-
-        @PutMapping("/{id}")
-        public ResponseEntity putAgendamento(@PathVariable Integer id,
-                                        @RequestBody Agendamento agendamentoAlterado) {
-            if (repository.existsById(id)) {
-                agendamentoAlterado.setFkQuadra(id);
-
-                repository.save(agendamentoAlterado);
-                return ResponseEntity.status(200).build();
-            }
-            return ResponseEntity.status(404).build();
-        }
-        @GetMapping("/recuperar-agendamentos/{fk_Quadra}")
-        public ResponseEntity getAgendamentosQuadra(@PathVariable Integer fk_Quadra) {
-            List<Agendamento> agendamentos = repository.findByFkQuadra(fk_Quadra);
-
-            if (agendamentos == null) {
-                return ResponseEntity.status(404).build();
-            }
-
-            return ResponseEntity.status(200).body(agendamentos);
+    @PostMapping
+    public ResponseEntity postAgendamento(@RequestBody Agendamento novoAgendamento) {
+        repository.save(novoAgendamento);
+        return ResponseEntity.status(201).build();
     }
 
-        @GetMapping("/relatorio/{id}")
-        public ResponseEntity getAgendamentoRelatorio(@PathVariable int id) {
-            if (repository.existsById(id)) {
-                List<Agendamento> agendamentoLista = repository.findAll();
-                Agendamento agendamento = repository.findById(id).get();
-                return ResponseEntity
-                        .status(200)
-                        .header("content-type", "plain/text")
-                        .body(String.format("\nRelatório do centros esportivos:\n  %s", agendamento.toString()));
+    @GetMapping("/{id}")
+    public ResponseEntity getAgendamento(@PathVariable Integer id) {
+        return ResponseEntity.of(repository.findById(id));
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteAgendamento(@PathVariable Integer id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
+    }
 
-            }
+    @PutMapping("/{id}")
+    public ResponseEntity putAgendamento(@PathVariable Integer id,
+                                         @RequestBody Agendamento agendamentoAlterado) {
+        if (repository.existsById(id)) {
+            agendamentoAlterado.setFkQuadra(id);
+
+            repository.save(agendamentoAlterado);
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
+    }
+
+    @GetMapping("/recuperar-agendamentos/{fk_Quadra}")
+    public ResponseEntity getAgendamentosQuadra(@PathVariable Integer fk_Quadra) {
+        List<Agendamento> agendamentos = repository.findByFkQuadra(fk_Quadra);
+
+        if (agendamentos == null) {
             return ResponseEntity.status(404).build();
         }
+
+        return ResponseEntity.status(200).body(agendamentos);
+    }
+
+    @GetMapping("/relatorio/{id}")
+    public ResponseEntity getAgendamentoRelatorio(@PathVariable int id) {
+        if (repository.existsById(id)) {
+            List<Agendamento> agendamentoLista = repository.findAll();
+            Agendamento agendamento = repository.findById(id).get();
+            return ResponseEntity
+                    .status(200)
+                    .header("content-type", "plain/text")
+                    .body(String.format("\nRelatório do centros esportivos:\n  %s", agendamento.toString()));
+
+
+        }
+        return ResponseEntity.status(404).build();
+    }
 
     @GetMapping(value = "/exportar-txt", produces = "plain/text")
     @ApiOperation(value = "Realiza a exportação de um arquivo com todos os centros esportivos")
@@ -154,6 +153,4 @@ public class AgendamentoController {
         }
         return ResponseEntity.status(404).build();
     }
-
-
 }
