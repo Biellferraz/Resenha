c0a68d96898b2a5f4bb563c5c9f636f6b69b39f3
Pedro_Leiva <phleiva@stefanini.com>
2022-04-08 22:51:01 -0300

    [mobile]Validacoes de cpf e celular

--- aplicacao/mobile/app/src/main/java/com/example/myapplication/TelaCadastro.kt
+++ aplicacao/mobile/app/src/main/java/com/example/myapplication/TelaCadastro.kt
@@ -3,13 +3,20 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.Mascaras.MascaraCPF
 
 class TelaCadastro : AppCompatActivity() {
+    private lateinit var et_cpf: EditText
+    private lateinit var et_telefone: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_cadastro)
-
+        et_cpf = findViewById(R.id.et_cpf)
+        et_cpf.addTextChangedListener(MascaraCPF.mask("###.###.###-##", et_cpf))
+        et_telefone = findViewById(R.id.et_telefone)
+        et_telefone.addTextChangedListener(MascaraCPF.mask("(##)#####-####", et_telefone))
     }
 
     fun login(v: View) {
