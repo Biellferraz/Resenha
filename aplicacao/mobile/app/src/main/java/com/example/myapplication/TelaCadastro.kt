c0661bf89ff4607d2c9050d15b61338ad40be789
Pedro_Leiva <phleiva@stefanini.com>
2022-04-08 21:29:08 -0300

    [mobile]Inicio das validacoes

--- aplicacao/mobile/app/src/main/java/com/example/myapplication/TelaCadastro.kt
+++ aplicacao/mobile/app/src/main/java/com/example/myapplication/TelaCadastro.kt
@@ -9,6 +9,7 @@ class TelaCadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_cadastro)
+
     }
 
     fun login(v: View) {
