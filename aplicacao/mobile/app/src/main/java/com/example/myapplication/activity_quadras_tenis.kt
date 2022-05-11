26b855c09383ae0e867ee82317f85183ccb0498f
Biellferraz <gabriel.ferraz101@gmail.com>
2022-05-11 20:42:42 -0300

    quadras de volei e tenis

---
+++ aplicacao/mobile/app/src/main/java/com/example/myapplication/activity_quadras_tenis.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_quadras_tenis : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quadras_tenis)
+    }
+}
