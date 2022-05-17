b533836867e55d7ad44ee73708767de0c8a52d21
Biellferraz <gabriel.ferraz101@gmail.com>
2022-05-17 07:08:54 -0300

    telas quadras tenis e volei

---
+++ aplicacao/mobile/app/src/main/java/com/example/myapplication/TelaQuadrasTenis.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaQuadrasTenis : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_quadras_tenis)
+    }
+}
